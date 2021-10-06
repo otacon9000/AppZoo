df94416fbdbecf001edc7311b1464d0c3aab8b2b
Andrea Alicino <andrea.alicino@gmail.com>
2021-10-06 11:50:50 +0200

    added cards

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text footer;
+    public Image photo;
+
+
+    public Card[] cards;
+
+
+
+
+   public void DisplayCard(int cardNumber)
+    {
+        title.text = cards[cardNumber].title;
+        description.text = cards[cardNumber].description;
+        footer.text = cards[cardNumber].footer;
+        photo.sprite = cards[cardNumber].photo;
+
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObject/Card.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card") ]
+public class Card : ScriptableObject
+{
+
+    public string title;
+    public string description;
+    public string footer;
+    public Sprite photo;
+    
+}
